f96fee4dc15400425d5bcc360093c2ee2b57bfbe
Matthew Smith <ssmitty67@gmail.com>
2015-02-22 12:56:50 -0600

    Starting to understand GitHub

--- cachematrix.R
+++ cachematrix.R
@@ -16,13 +16,19 @@ makeCacheMatrix <- function(x = matrix()) {
   list(set = set, get = get,
        setmean = setmean,
        getmean = getmean)
-  
-
 }
-
+  
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
 }
